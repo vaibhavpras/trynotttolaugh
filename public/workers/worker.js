d8dafeb7b5a18d73bd0b9163e141bb590bd7720c
Vaibhav P <vaibav.prasanna@gmail.com>
2021-01-10 18:34:45 +0530

    smile detection working
    smile detection feture works with no errors. revised the app architecture. brought the detection feature from the web worker to the main thread.

--- public/workers/worker.js
+++
@@ -1,9 +0,0 @@
-
-this.onmessage = (e) => {
-  console.log("message in worker.js");
-  this.postMessage('camera inited in worker')
-
-  //initCamera(640, 480).then(_ => console.log('camera initialized in worker'));
- 
-   
-};
--- src/TestComponent.js
+++ src/TestComponent.js
@@ -1,48 +1,104 @@
-import React, {useEffect, useRef} from 'react'
+import React, { useEffect, useRef } from "react";
+import * as faceapi from "face-api.js";
 
 function TestComponent() {
+  const video = useRef();
+  const counter = useRef();
+  var temp;
+  var isStarted = false;
 
-    const cam = useRef();
+  const initCamera = async (width, height) => {
+    console.log("initing camera");
+    video.current.width = width;
+    video.current.height = height;
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        facingMode: "user",
+        width: width,
+        height: height,
+      },
+    });
+    video.current.srcObject = stream;
+    return new Promise((resolve) => {
+      video.current.onloadedmetadata = () => {
+        resolve(video);
+      };
+    });
+  };
 
-    const initCamera = async (width, height) => {
-        console.log('initing camera')
-        cam.current.width = width;
-        cam.current.height = height;
-        const stream = await navigator.mediaDevices.getUserMedia({
-          audio: false,
-          video: {
-            facingMode: "user",
-            width: width,
-            height: height
+  useEffect(() => {
+    initCamera(320, 240).then((video) => {
+      console.log("Camera was initialized");
+    });
+
+    let rand = 0;
+    setInterval(() => {
+      counter.current.innerHTML = rand;
+      rand++;
+    }, 100);
+  });
+
+  const start = () => {
+    isStarted = true;
+    const MODEL_URL = process.env.PUBLIC_URL + "/models";
+
+    Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+      faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+      faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+      faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+    ]).then(() => {
+      console.log("started reading");
+      if (!temp) {
+        temp = setInterval(async () => {
+          let happiness;
+
+          try {
+            const detections = await faceapi
+              .detectSingleFace(
+                video.current,
+                new faceapi.TinyFaceDetectorOptions()
+              )
+              .withFaceExpressions();
+            if (detections.expressions.hasOwnProperty("happy")) {
+              happiness = detections.expressions.happy;
+            }
+
+            if (happiness > 0.7) {
+              console.log("you smiled!");
+            }
+          } catch (e) {
+            console.log(e);
           }
-        });
-        cam.current.srcObject = stream;
-        return new Promise((resolve) => {
-          cam.current.onloadedmetadata = () => {
-            resolve(cam);
-      }});
-    }
+          // const resizedDetections = faceapi.resizeResults(detections, displaySize)
+          // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+          // faceapi.draw.drawDetections(canvas, resizedDetections)
+          // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+          // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+        }, 100);
+      }
+    });
+  };
 
-    useEffect(() => {
-        const worker = new Worker('./workers/worker.js')
-        worker.postMessage('Hello Worker')
-        worker.onmessage = e => {
-        console.log('Message received from worker:', e.data)
+  return (
+    <div>
+      <h1 ref={counter}></h1>
+      <button onClick={start}>start</button>
+      <button
+        onClick={() => {
+          if (isStarted) {
+            clearInterval(temp);
+          }
+          isStarted = false;
+          temp = false
+        }}
+      >
+        stop
+      </button>
+      <video ref={video} autoPlay muted playsInline></video>
+    </div>
+  );
+}
 
-        initCamera(640, 480)
-        .then(video => {
-        console.log('Camera was initialized');
-        });
-    }
-    }
-    );
-
-    return (
-        <div>
-            <video ref={cam} autoPlay muted playsInline></video>
-        </div>
-    )
-    }
-
-
-export default TestComponent
+export default TestComponent;
