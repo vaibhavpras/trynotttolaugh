04a48f815e5219e01c4b346bb2cc1518bbeddfd6
Vaibhav Prasanna <vaibhav.prasanna@outlook.com>
2021-05-27 22:42:53 +0530

    Revamped UI of StartComponent -Got rid of Neumorphic design style

--- src/StartComponent.js
+++ src/StartComponent.js
@@ -1,9 +1,12 @@
 import { React, useState } from "react";
 import { useHistory } from "react-router-dom";
-import './StartComponent.css';
+import "./StartComponent.css";
 
 function StartComponent() {
-  const [selectState, selectStateSetter] = useState("Any");
+  const [selectState, selectStateSetter] = useState({
+    label: "Any",
+    desc: "all kinds of jokes",
+  });
   const history = useHistory();
 
   const handleSubmit = function (e) {
@@ -11,44 +14,72 @@ function StartComponent() {
     history.replace({
       pathname: "/game",
       state: {
-        type: selectState,
+        type: selectState.label,
       },
     });
   };
-  
+
   const handleChange = (e) => {
-    selectStateSetter(e.target.value);
+    switch (e.target.value) {
+      case "Any":
+        selectStateSetter({
+          label: e.target.value,
+          desc: "all kinds of jokes",
+        });
+        break;
+      case "Programming":
+        selectStateSetter({
+          label: e.target.value,
+          desc: "programming jokes",
+        });
+        break;
+      case "Pun":
+        selectStateSetter({ label: e.target.value, desc: "puns" });
+        break;
+      case "Dark":
+        selectStateSetter({ label: e.target.value, desc: "dark jokes" });
+        break;
+    }
   };
 
   return (
-    <div className='outer-container'>
+    <div className = "outer-container"><div className="inner-container">
+      <div className="card">
+        <p className="title">YOU LAUGH, YOU LOSE</p>
 
-      <div className='title'>YOU LAUGH, YOU LOSE</div>
-
-      <div className='prompt'>
-
-      <form onSubmit={handleSubmit}>
-      <label className='label'> I find </label>
-        <select className="dropdown" value={selectState} onChange={handleChange} >
-          <option value="Any">all kinds of</option>
-          <option value="Programming">programming jokes</option>
-          <option value="Pun">puns</option>
-          <option value="Dark">dark jokes</option>
-        </select>
-
-        <label className='label'> funny. </label>
-
-        <div className='submit-btn-container'><input type="submit" value="start" className='submit-btn'></input></div>
-
-      </form>
-
+        <div className="prompt">
+          <form onSubmit={handleSubmit} id = "startForm">
+            <label> I find </label>
+            <select
+              className="dropdown"
+              value={selectState.label}
+              onChange={handleChange}
+              style={{ width: `${0.5 * selectState.desc.length + 0.9}em` }}>
+              <option value="Any">all kinds of jokes</option>
+              <option value="Programming">programming jokes</option>
+              <option value="Pun">puns</option>
+              <option value="Dark">dark jokes</option>
+            </select>
+            <label> funny. </label>
+          </form>
+          </div>
+          <div className="submit-btn-container">
+              <input form = "startForm" type="submit" value="start" className="submit-btn"></input>
+            </div>
       </div>
 
-      <div className='disclaimer'>
-      <p>Disclaimer: You need to enable your web camera to be able to play this game.</p>
-      <p>Also note, this site does not connect to a backed server, which means your video feed is analyzed only inside your browser and the data stays only on your device. </p>
+      <div className="disclaimer">
+        <p>
+          Disclaimer: You need to enable your web camera to be able to play this
+          game.
+        </p>
+        <p>
+          Also note, this site does not connect to a backend server, which means
+          your video feed is analyzed only inside your browser and the data
+          stays only on your device.
+        </p>
       </div>
-
+    </div>
     </div>
   );
 }
